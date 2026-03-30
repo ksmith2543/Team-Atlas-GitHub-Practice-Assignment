a0751aad3b95aea2331141326758daec6bd5d135
Kevin Smith <ksmith2543@gmail.com>
2026-03-30 14:14:24 -0500

    Add files via upload

---
+++ GitHubDemo.java
@@ -0,0 +1,41 @@
+public class GitHubDemo {
+
+    public static int sum(int x, int y){
+        //Function completed by ???
+        int sum = 0;
+        //complete me
+        return sum;
+    }
+
+    public static int subtract(int x, int y){
+        //Function completed by ???
+        int difference = 0;
+        //complete me
+        return difference;
+    }
+
+    public static int multiply(int x, int y){
+        //Function completed by ???
+        int product = 0;
+        //complete me
+        return product;
+    }
+
+    public static float divide(int x, int y){
+        //Function completed by ???
+        float quotient = 0;
+        //complete me
+        return quotient;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Atlas GitHub sample project.");
+        int x = 3;
+        int y = 5;
+        System.out.println(x + " + " + y + " = " + sum(x, y));
+        System.out.println(x + " - " + y + " = " + subtract(x, y));
+        System.out.println(x + " * " + y + " = " + multiply(x, y));
+        System.out.printf("%d / %d = %.2f", x, y, divide(x,y));
+    }
+
+}
